25e05539a24187755ba0bab6297b3fb534066363
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-01 10:09:05 -0300

    se limpia codigo

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { conectarDB } from './src/database/db.js'; 
-import colorRoutes from './src/routes/color.routes.js'; 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { conectarDB } from "./src/database/db.js";
+import colorRoutes from "./src/routes/color.routes.js";
 dotenv.config();
 
 const app = express();
 
-// Conectar a la base de datos
 conectarDB();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
-
-// Rutas API
-app.use('/api/colores', colorRoutes);
+app.use("/api/colores", colorRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
